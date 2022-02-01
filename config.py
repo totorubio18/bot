ba09578ba59fca7d6ba6090c377cc046d8ca4cc9
totorubio18 <98791222+totorubio18@users.noreply.github.com>
2022-01-31 21:27:23 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+BOT_TOKEN = '5008274650:AAFt19uHzTC88-L6B8oPfA7z2__iVtX-vys' #token de tu bot
+API_ID = 15374509 # lo creas en https://my.telegram.org/
+API_HASH = '00e43dbb6632a11be01895d2c5db6af0'  # lo creas en https://my.telegram.org/
+OWNER = [1067871346]
+
+# DATOS DEL MOODLE
+USUARIO = 'pubg'
+MOODLE_PASSWORD = '@Pubg123'
+USUARIO_ID = 2789 #Tu id
+ZIP_MB = 50
+MOODLE_URL = 'moodle.uccfd.cu'
+
+# CUENTA DE MEGA - este bot sirve par mega tambien
+GMAIL_MEGA = 'AQUI TU GMAIL' 
+PASS_MEGA = 'AQUI TU PASS'
